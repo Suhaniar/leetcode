22051e9ba857798a3daa8a38a464f60a3d72d555
Suhaniar <suhaniar19@gmail.com>
2026-06-14 13:38:01 +0530

    Time: 1 ms (100%), Space: 43.7 MB (75.07%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,42 +1,30 @@
 class Solution {
     public int myAtoi(String s) {
+            int i=0,n=s.length();
 
-        int i = 0, n = s.length();
-
-        // Skip leading spaces
-        while (i < n && s.charAt(i) == ' ') {
+       while(i<n && s.charAt(i)==' '){
             i++;
+       }
+       int sign=1;
+       if(i<n &&(s.charAt(i)=='+'|| s.charAt(i)=='-')){
+        if(s.charAt(i)=='-'){
+            sign=-1;
         }
+        i++;
+       }
+       int result=0;
+       while(i<n && Character.isDigit(s.charAt(i))){
+        int digit=s.charAt(i)-'0';
 
-        // Sign
-        int sign = 1;
-        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-            if (s.charAt(i) == '-') {
-                sign = -1;
-            }
-            i++;
+        if(result>Integer.MAX_VALUE/10||(result==Integer.MAX_VALUE/10&& digit>Integer.MAX_VALUE%10)){
+            return sign==1? Integer.MAX_VALUE:Integer.MIN_VALUE;
         }
+        result=result*10+digit;
+        i++;
+       }
+       return result*sign ;
+       
 
-        int result = 0;
 
-        while (i < n && Character.isDigit(s.charAt(i))) {
-
-            int digit = s.charAt(i) - '0';
-
-            // Overflow check
-            if (result > Integer.MAX_VALUE / 10 ||
-               (result == Integer.MAX_VALUE / 10 &&
-                digit > Integer.MAX_VALUE % 10)) {
-
-                return sign == 1
-                        ? Integer.MAX_VALUE
-                        : Integer.MIN_VALUE;
-            }
-
-            result = result * 10 + digit;
-            i++;
-        }
-
-        return result * sign;
     }
 }
