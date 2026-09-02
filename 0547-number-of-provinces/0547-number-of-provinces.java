fd44d3ed266f831cf6b8e6a0061774d56fd3f343
Suhaniar <suhaniar19@gmail.com>
2026-09-02 09:58:49 +0530

    Time: 1 ms (86.56%), Space: 47.1 MB (74.85%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,29 +1,31 @@
-class Solution { 
-    int count = 0; 
-    boolean[] visited; 
-
-    public int findCircleNum(int[][] isConnected) { 
-        
-        int n = isConnected.length;
-        visited = new boolean[n];
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
+class Solution {
+   
+    static boolean[] visited;
+    static Queue<Integer> q=new LinkedList<>();
+    public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length;
+        visited=new boolean[n];
+        int count=0;
+    for(int i=0;i<n;i++){
+            if(!visited[i]){
+               // count++;
+                bfs(i,isConnected);
                 count++;
-                dfs(i, isConnected);
             }
-        }
-
-        return count;
     }
-
-    void dfs(int city, int[][] isConnected) {
-        visited[city] = true;
-
-        for (int i = 0; i < isConnected.length; i++) {
-            if (isConnected[city][i] == 1 && !visited[i]) {
-                dfs(i, isConnected);
+    return count;
+    }
+    public static void bfs(int start,int[][] isConnected){
+            visited[start]=true;
+            q.offer(start);
+            while(!q.isEmpty()){
+                int current=q.poll();
+            for(int i=0;i<isConnected.length;i++){
+                if(isConnected[current][i]==1 && !visited[i]  ){
+                    visited[i]=true;
+                    q.offer(i);
+                }
             }
-        }
+            }
     }
 }
